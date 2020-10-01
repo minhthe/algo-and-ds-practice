4c10594b82f767aabd2f401533a156a55340ded5
thetran <minhthe.007@gmail.com>
2020-10-01 11:37:03 +0000

    01/31 of october

---
+++ leetcode/explore/october/01_findRequest.py
@@ -0,0 +1,16 @@
+import collections
+class RecentCounter:
+
+    def __init__(self):
+        self.que = collections.deque([])
+
+    def ping(self, t: int) -> int:
+        self.que.append(t)
+        while self.que[0] < t - 3000:
+            self.que.popleft()
+        return len(self.que)
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
